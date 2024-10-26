6e70255aec062f9940ebfe0e0a2c4b0156cb6a55
Will Daly <will.e.daly@gmail.com>
2024-10-25 22:13:24 -0700

    Event and drawing loop

--- port/main.cpp
+++ port/main.cpp
@@ -4,19 +4,41 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const Uint32 TARGET_TICKS_PER_FRAME = 16; // About 60fps
+
+void runEventLoop(SDL_Window *window) {
+  SDL_Event e;
+  bool quit = false;
+  while (!quit) {
+    Uint32 startTicks = SDL_GetTicks(); // milliseconds
+
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glFlush();
+    SDL_GL_SwapWindow(window);
+
+    Uint32 endTicks = SDL_GetTicks(); // milliseconds
+    Uint32 elapsedTicks = endTicks - startTicks;
+    if (elapsedTicks < TARGET_TICKS_PER_FRAME) {
+      SDL_Delay(TARGET_TICKS_PER_FRAME - elapsedTicks);
+    }
+  }
+}
 
 int main(int argc, char *args[]) {
-  SDL_Window *window = NULL;
-  SDL_Surface *screenSurface = NULL;
-
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     return 1;
   }
 
-  window = SDL_CreateWindow("GLFighters", SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                            SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+  SDL_Window *window = SDL_CreateWindow("GLFighters", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                        SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
   if (window == NULL) {
     printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     return 1;
@@ -29,19 +51,7 @@ int main(int argc, char *args[]) {
   }
 
   SDL_GL_MakeCurrent(window, glctx);
-  SDL_GL_SwapWindow(window);
-
-  glClear(0);
-
-  SDL_Event e;
-  bool quit = false;
-  while (quit == false) {
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT)
-        quit = true;
-    }
-  }
-
+  runEventLoop(window);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
