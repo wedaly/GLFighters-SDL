531190f57c2474801bbc50102069158111ffab98
Will Daly <will.e.daly@gmail.com>
2024-11-08 19:10:40 -0800

    Fix C++ warnings in game.cpp

--- port/print.h
+++ port/print.h
@@ -2,4 +2,4 @@
 
 bool createFont(int textureID);
 void freeFont();
-void printToScreen(int x, int y, char *s, bool italicize, float size, int screenwidth, int screenheight);
+void printToScreen(int x, int y, const char *s, bool italicize, float size, int screenwidth, int screenheight);
